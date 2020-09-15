f78afd81b2733120d12275a787e83097e47ba542
Lucas <estebeweb@gmail.com>
2020-09-15 18:12:28 +0200

    :ambulance: Router Hotfix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route,} from 'react-router-dom'
 
 import {TopNav} from "./components/layout/TopNav";
 import {LeftNav} from "./components/layout/LeftNav";
@@ -18,11 +18,10 @@ function App() {
           <div className={"row"}>
           <LeftNav/>
           <div className={"container"}>
-
             <Route path={"/saisirNom"} component={NameForm}/>
             <Route path={"/choisirSexe"} component={SexForm}/>
             <Route path={"/afficherPatient"} component={Patient}/>
-
+            <Route exact path={"/"} component={NameForm}/>
           </div>
           </div>
         </div>
