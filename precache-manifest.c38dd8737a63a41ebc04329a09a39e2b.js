e888a28599b2cb883d9fb390461a6556da0654b5
Lucas <estebeweb@gmail.com>
2020-09-15 20:35:55 +0200

    Updates

---
+++ precache-manifest.c38dd8737a63a41ebc04329a09a39e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e76297e1b9e502f0729538845fe752",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "73381be9218e6e152106",
+    "url": "/test-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "47e22076b04b9204c5e9",
+    "url": "/test-react/static/js/2.37fb02a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-react/static/js/2.37fb02a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73381be9218e6e152106",
+    "url": "/test-react/static/js/main.f26ffc80.chunk.js"
+  },
+  {
+    "revision": "749a69bc2f1e6da32b80",
+    "url": "/test-react/static/js/runtime-main.0b88a9d2.js"
+  }
+]);
